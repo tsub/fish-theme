f8ca5841b11a4dcf718949573d508692063446e7
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-08-04 01:07:30 +0900

    Show $AWS_PROFILE in right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,2 +1,13 @@
+function prompt_aws_profile
+  if [ -z "$AWS_PROFILE" ]
+    return
+  end
+
+  echo (set_color yellow)" $AWS_PROFILE"(set_color normal)
+end
+
 function fish_right_prompt
+  set -l aws (prompt_aws_profile)
+
+  echo "$aws"
 end
